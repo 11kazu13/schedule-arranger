4e1320b8f23185a13511b05ed375fae3dfc1832f
Kazu <kazuki.ino01@gmail.com>
2026-01-11 14:50:52 +0900

    コメントについてのパスパラメータのバリデータと、JSON バリデータを定義

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -2,8 +2,41 @@ const { Hono } = require('hono');
 const ensureAuthenticated = require('../middlewares/ensure-authenticated');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({ log: ['query'] });
+const { z } = require('zod');
+const { zValidator } = require('@hono/zod-validator');
 
 const app = new Hono();
+
+const paramValidator = zValidator(
+  'param',
+  z.object({
+    scheduleId: z.string().uuid(),
+    userId: z.coerce.number().int().min(0)
+  }),
+  (result, c) => {
+    if (!result.success) {
+      return c.json({
+        status: 'NG',
+        error: [result.error]
+      }, 400)
+    }
+  }
+);
+
+const jsonValidator = zValidator(
+  'json',
+  z.object({
+    comment: z.string().min(0).max(255)
+  }),
+  (result, c) => {
+    if (!result.success) {
+      return c.json({
+        status: 'NG',
+        error: [result.error]
+      }, 400)
+    }
+  }
+);
 
 app.post(
   '/:scheduleId/users/:userId/comments',
